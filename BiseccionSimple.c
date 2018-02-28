13529136988f95016674e0dabd79ee8909cd0e03
Diego <darkzeus@gmx.es>
2018-02-28 09:58:10 +0100

    Update BiseccionSimple.c

--- BiseccionSimple.c
+++ BiseccionSimple.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 
 
 void main() {
 	float a = 0;
 	float b = 0;
-	int error = 0;
+	float error = 0;
 	float P;
 	float x = 0;
 	float y = 0;
@@ -15,76 +16,58 @@ void main() {
 	int grado = 0;
 	int contador = 0;
 
-	//Posible añadido con nuevas funciones para añadir un grado de error
-
-	printf("La funcion es:  x^3 -x^2 - x + 5\n");
-
+	printf("La funcion es:  x^3 -x^2 - x - 5\n");
 	printf("Por favor, introduce dos valores\n");
-
 	scanf_s("%f", &a);
 	scanf_s("%f", &b);
 
-	printf("Introduce un grado de error (2^n)\n");
-
-	scanf_s("%d", &grado);
-
 	x = a;
-	ya = x*x*x -x*x - x + 5;
+	ya = x * x*x - x * x - x - 5;
 
 	x = b;
-	yb = x*x*x - x * x - x + 5;
-
-	
+	yb = x * x*x - x * x - x - 5;
 
 	if (ya * yb > 0) {
 		printf("Los valores introducidos no son adecuados, introduzca dos valores validos por favor:\n");
-
 		scanf_s("%f", &a);
 		scanf_s("%f", &b);
-
 		x = a;
-		ya = x*x*x - x * x - x + 5;
-
+		ya = x * x*x - x * x - x - 5;
 		x = b;
-		yb = x*x*x - x * x - x + 5;
-	}
+		yb = x * x*x - x * x - x - 5;
+		}
+	printf("Introduce un grado de error (2^n)\n");
+	scanf_s("%d", &grado);
 
+	for (contador = 0;  contador < grado; contador++) {
 
-	for(contador<grado;contador++){
-		
 		printf("Los valores son: a = %f || b = %f\n", a, b);
 
 		P = (a + b) / 2;
 		//funcion
-		y = x * x*x - x * x - x + 5;
+		y = x * x*x - x * x - x - 5;
 
 		x = a;
-		ya = x * x*x - x * x - x + 5;
+		ya = x * x*x - x * x - x - 5;
 
 		x = b;
-		yb = x * x*x - x * x - x + 5;
+		yb = x * x*x - x * x - x - 5;
 
 		x = P;
-		y = x * x*x - x * x - x + 5;
+		y = x * x*x - x * x - x - 5;
 
-		error = (b - a) / (1 << error + 1);
-
+		error = (b - a) / pow(2,error);
 
 		if (ya * y < 0) {
 			b = P;
-		}
+			}
 		else {
 			a = P;
+			}
 		}
-	}
-	
-	printf("El punto medio es %f\nLos nuevos valores son: %f y %f\nError aproximado de: |%d|\n", P, a , b, error);
 
+	printf("El punto medio es %f\nLos nuevos valores son: %f y %f\nError aproximado de: |%f|\n", P, a, b, error);
 
 	system("PAUSE");
 	return 0;
 }
-
-
-
-
